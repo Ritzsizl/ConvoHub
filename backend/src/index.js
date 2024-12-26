5390cd129c47a516fe64053f47ee99914a46180e
Ritzsizl <ritzraha4u@gmail.com>
2024-12-26 22:17:54 +0530

    signup api route

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,9 +8,12 @@ dotenv.config();
 const app = express();
 
 
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
+const PORT = process.env.PORT ;
 
 app.use('/api/auth', authRouter )
+
+
 app.listen( PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
