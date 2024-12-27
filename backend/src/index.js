cc424841384e52a68d455a79b3d7454ad1bbf3e5
Ritzsizl <ritzraha4u@gmail.com>
2024-12-27 21:46:46 +0530

    message section and login logout route

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,16 +2,21 @@ import express from 'express';
 import authRouter from './routes/auth.route.js';
 import dotenv from 'dotenv';
 import { connectDB } from './lib/db.js';
+import cookieParser from 'cookie-parser';
+import messageRouter from "./routes/message.route.js"
 
 dotenv.config();
 
 const app = express();
 
-
 app.use(express.json());
+app.use(cookieParser());
+
+
 const PORT = process.env.PORT ;
 
 app.use('/api/auth', authRouter )
+app.use('/api/message', messageRouter )
 
 
 app.listen( PORT, () => {
