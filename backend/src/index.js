0ea84c0a12f685bdc506662182ee83f99fbe6753
Ritzsizl <ritzraha4u@gmail.com>
2024-12-31 12:40:48 +0530

    Project Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,11 +5,13 @@ import cors from "cors";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import messageRouter from "./routes/message.route.js";
-import {app, server} from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,9 +22,16 @@ app.use(
   })
 );
 
-
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+  })
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
